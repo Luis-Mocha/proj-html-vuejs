4b7dae3d89a8b4d1092f9c885f150e016f95a0ac
Luis-Mocha <luismocha97@gmail.com>
2023-05-12 16:48:42 +0200

    struttura base header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,14 +3,46 @@
     export default {
         name: 'AppHeader',
     }
-    
+
 </script>
 
 
 <template>
 
     <header>
-        <h2>TECH FORUMS</h2>
+        <div class="container d-flex justify-content-between">
+            <a href="#">
+                <img src="/img/logo.png" alt="Logo Avada">
+            </a>
+            <nav class="navbar navbar-expand-md">
+                <!-- Bottone per aprire il menu -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-menu" aria-controls="nav-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- menu -->
+                <div class="collapse navbar-collapse" id="nav-menu">
+
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+
+                    <div class="header-input d-flex" role="search">
+                        <input class="" type="search" placeholder="Search" aria-label="Search">
+                        <button class="">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+
+                </div>
+            </nav>
+
+        </div>
     </header>
 
 </template>
@@ -23,7 +55,16 @@
 
     header {
         background-color: $blue-estern;
-        height: 100px;
+        // height: 100px;
+        padding: 10px 0;
+
+        .header-input {
+            margin-left: 20px;
+
+            input {
+                width: 100px;
+            }
+        }
     }
 
 
