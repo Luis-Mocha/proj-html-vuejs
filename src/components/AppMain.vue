bd369c10cd0783bda49e0b457e810ca235353751
Luis-Mocha <luismocha97@gmail.com>
2023-05-13 16:56:15 +0200

    struttura news card e featured article

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -41,20 +41,116 @@
         </p>
     </div>
 
-    <div class="main-section container">
-        <div class="main-name">
-            <p>
-                Phasellus eget metus
-            </p>
+    <div id="news-section" class="main-section">
+        <div class="container">
 
-            <h2>
-                All the latest news
-            </h2>
+            <div class="main-name">
+                <p>
+                    Phasellus eget metus
+                </p>
 
-            <hr>
+                <h2>
+                    All the latest news
+                </h2>
+
+                <hr>
+            </div>
+
+            <div class="main-body">
+                <div class="news-row">
+
+                    <div class="news-card">
+                        <img src="img\post_feat_img_23-320x202.jpg" alt="Foto Articolo">
+
+                        <div class="card-body">
+                            <div class="card-title">loreskjhda asuhdakjhsd uhasd</div>
+                            <div class="card-subtitle mb-2 text-body-secondary">Card subtitle</div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+
+                    <div class="news-card">
+                        <img src="img\post_feat_img_23-320x202.jpg" alt="Foto Articolo">
+
+                        <div class="card-body">
+                            <div class="card-title">loreskjhda asuhdakjhsd uhasd</div>
+                            <div class="card-subtitle mb-2 text-body-secondary">Card subtitle</div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+
+                    <div class="news-card">
+                        <img src="img\post_feat_img_23-320x202.jpg" alt="Foto Articolo">
+
+                        <div class="card-body">
+                            <div class="card-title">loreskjhda asuhdakjhsd uhasd</div>
+                            <div class="card-subtitle mb-2 text-body-secondary">Card subtitle</div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="featured-article">
+                    <div class="container-featured">
+
+                        <span>Featured Article</span>
+
+                        <h3>
+                            Cras Malesuada et orci eget pharetra
+                        </h3>
+
+                        <p>
+                            In quis lectus sed leo elemntum faucibus in dapibus dictum. Nulla molestie tortor nec lectus venenatis,sed blandit dui, dolor at bibendum sadips ets ipsum dolores ficilis uns leo lectus.
+                        </p>
+
+                        <div class="my-btn btn-black">
+                            Read more  >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="news-row">
+
+                    <div class="news-card">
+                        <img src="img\post_feat_img_23-320x202.jpg" alt="Foto Articolo">
+
+                        <div class="card-body">
+                            <div class="card-title">loreskjhda asuhdakjhsd uhasd</div>
+                            <div class="card-subtitle mb-2 text-body-secondary">Card subtitle</div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+
+                    <div class="news-card">
+                        <img src="img\post_feat_img_23-320x202.jpg" alt="Foto Articolo">
+
+                        <div class="card-body">
+                            <div class="card-title">loreskjhda asuhdakjhsd uhasd</div>
+                            <div class="card-subtitle mb-2 text-body-secondary">Card subtitle</div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+
+                    <div class="news-card">
+                        <img src="img\post_feat_img_23-320x202.jpg" alt="Foto Articolo">
+
+                        <div class="card-body">
+                            <div class="card-title">loreskjhda asuhdakjhsd uhasd</div>
+                            <div class="card-subtitle mb-2 text-body-secondary">Card subtitle</div>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-
     </div>
+        <!-- http://newsapi.org/v2/top-headlines?q=iphone&country=it&category=technology&language=it&pageSize=10&apiKey= -->
+
+        <!-- https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=4396876dd597418790439730f9024c47 -->
+        <!-- https://newsapi.org/v2/top-headlines?q=mac&category=technology&apiKey=4396876dd597418790439730f9024c47 -->
+
+        <!-- https://newsapi.org/v2/everything?q=tech&pageSize=20&language=it&apiKey=4396876dd597418790439730f9024c47 -->
 
 </template>
 
@@ -102,5 +198,33 @@
         }
     }
 
+    // --- SEZIONE NEWS
+    .news-row {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 0;
+
+        .news-card {
+            width: calc(100% / 3 - 20px);
+
+            img {
+                width: 100%;
+            }
+        }
+    }
+
+    #featured-article {
+        color: white;
+        height: 300px;
+        background-image: url("img/featured_article_1_bg.jpg");
+        background-size: cover;
+        padding: 10%;
+        display: flex;
+        align-items: center;
+
+        .container-featured{
+            
+        }
+    }
 
 </style>
