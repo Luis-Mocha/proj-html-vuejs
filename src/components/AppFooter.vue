e9d7c7d83a021473a1c9a8a879f5c6e9f23da62c
Luis-Mocha <luismocha97@gmail.com>
2023-05-15 15:33:03 +0200

    footer sezione social html e scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,6 +11,8 @@
 <template>
 
     <footer>
+
+        <!-- menus footer -->
         <div class="container">
             <div class="menu-footer">
                 <!-- Colonna 1 -->
@@ -39,6 +41,38 @@
                 </div>
                 
 
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- social footer -->
+        <div class="container">
+            <div class="social-footer">
+                <div id="info-sito" class="col-12 col-md-8 d-md-flex">
+                    <span> © Copyright 2012-2020</span>
+                    <span>|</span>
+                    <span> Avada Team by <a href="">Theme Fusione</a></span>
+                    <span>|</span>
+                    <span> All Rights Reserved</span>
+                    <span>|</span>
+                    <span> Powered by <a href="">WordPress</a> </span>
+                </div>
+
+                <div id="social-links" class="col-12 col-md-4 justify-content-evenly justify-content-md-end mt-3 mt-md-0">
+                    <a href="" class="ms-md-3">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="" class="ms-md-3">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="" class="ms-md-3">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="" class="ms-md-3">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </footer>
@@ -74,6 +108,56 @@
                 
                 span {
                     margin-right: 5px;
+                }
+            }
+        }
+    }
+
+    .social-footer {
+        padding: 30px 0;
+        display: flex;
+        flex-wrap: wrap;
+
+        #info-sito {
+            align-items: center;
+
+            span {
+                margin-right: 5px ;
+
+                a {
+                    font-weight: 600;
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                }
+            }
+            
+        }
+
+        #social-links {
+            display: flex;
+            align-items: center;
+
+            a {
+                i {
+                    color: white;
+                    line-height: 50px;
+                    width: 50px;
+                    height: 50px;
+                    text-align: center;
+                    border-radius: 50%;
+                }
+                .fa-facebook-f {
+                    background-color: rgb(60,89,152);
+                }
+                .fa-twitter {
+                    background-color: rgb(84,172,238) ;   
+                }
+                .fa-instagram {
+                    background-color: rgb(63,114,155);
+                }
+                .fa-youtube {
+                    background-color: red;
                 }
             }
         }
